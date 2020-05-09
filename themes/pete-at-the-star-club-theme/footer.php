7cf48822432b734fa57cf7818b718a27a013c1a9
Hinrich Riedel <hinrich.riedel@joblift.de>
2020-05-09 21:12:44 +0200

    v1

--- themes/pete-at-the-star-club-theme/footer.php
+++ themes/pete-at-the-star-club-theme/footer.php
@@ -12,10 +12,19 @@
 
 ?>
 
-
-<p style="text-align:center;"><a href="/impressum.html">Impressum</a></p>
-
-
+<div class="grid_12" style="padding-top:20px;">
+    <p style="text-align:center;">
+        <?php if (is_page('impressum')) : ?>
+        <a href="/">zurück</a>
+        <?php
+        else :
+        ?>
+        <a href="<?php echo get_page_link(get_page_by_path('impressum')); ?>">Impressum</a>
+        <?php
+        endif;
+        ?>
+    </p>
+</div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- themes/pete-at-the-star-club-theme/static.php
+++ themes/pete-at-the-star-club-theme/static.php
@@ -1,3 +1,5 @@
+<?php /* Template Name: Static Page With Sidebar */ ?>
+
 <?php
 
 /**
@@ -16,36 +18,33 @@
 get_header();
 ?>
 <main class="grid_8" id="blog">
-    <?php include('blog.php'); ?>
+    <?php
+    $page = get_page_by_title('page-impressum');
+    $content = apply_filters('the_content', $page->post_content);
+    echo $content;
+    ?>
+    <h1><?php the_title(); ?></h1>
+    <?php
+    // TO SHOW THE PAGE CONTENTS
+    while (have_posts()) : the_post(); ?>
+    <!--Because the_content() works only inside a WP Loop -->
+    <div class="entry-content-page">
+        <?php the_content(); ?>
+        <!-- Page Content -->
+    </div><!-- .entry-content-page -->
+    <?php
+    endwhile; //resetting the page loop
+    ?>
 </main>
 <aside class="grid_4">
     <?php include('logo.php'); ?>
     <?php include('social.php'); ?>
-    <?php include('embeds.php'); ?>
     <?php include('bandinfo.php'); ?>
     <?php include('kontakt.php'); ?>
     <?php include('press.php'); ?>
 </aside>
 <!-- end .grid_12 -->
 <div class="clear"></div>
-
-<!-- GOOGLE ANALYTICS -->
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36385486-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
-        '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-</script>
-<!-- GOOGLE ANALYTICS -->
 
 
 <?php get_footer(); ?>
